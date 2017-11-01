973e89720fa48ef479423e8677a006a6ab1511bd
Shah <nirmal.shah@here.com>
2017-11-02 01:31:36 +0530

    modify for failure

--- src/test/java/packtselenium/chap3_2/MainTest.java
+++ src/test/java/packtselenium/chap3_2/MainTest.java
@@ -22,7 +22,7 @@ public class MainTest {
 	@Test
 	public void verify(){
 		driver.get("http://www.seleniumhq.org");
-		String expectedTitle = "Selenium - Web Browser Automation";
+		String expectedTitle = "Selenium - Web Browser Automation12";
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(expectedTitle, actualTitle,"wrong title");
 	}
