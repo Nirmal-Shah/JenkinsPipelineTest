23f9cd712221fd668051e8b2c74d2c6773377e98
Shah <nirmal.shah@here.com>
2017-11-13 18:12:05 +0530

    changes made for linux

--- src/test/java/packtselenium/chap3_2/MainTest.java
+++ src/test/java/packtselenium/chap3_2/MainTest.java
@@ -1,9 +1,12 @@
 package packtselenium.chap3_2;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,18 +17,28 @@ public class MainTest {
 	
 	@BeforeTest
 	public void setUp(){
+		ChromeDriverService service;
 		File file = new File(System.getProperty("user.dir")+"//resources//drivers//chromedriver");
 		file.setExecutable(true);
 		file.setReadable(true);
 		file.setWritable(true);
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//resources//drivers//chromedriver");
-		driver = new ChromeDriver();
+		service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(file)
+                .usingAnyFreePort()
+                .build();
+		try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+		driver = new RemoteWebDriver(service.getUrl(), DesiredCapabilities.chrome());
 		driver.manage().window().maximize();
 	}
 	
 	@Test
 	public void verify(){
-		driver.get("http://localhost:8070/SimpleWebApp/");
+		driver.get("http://www.seleniumhq.org/");
 		String expectedTitle = "Selenium - Web Browser Automation";
 		String actualTitle = driver.getTitle();
 		Assert.assertEquals(actualTitle,expectedTitle,"wrong title");
